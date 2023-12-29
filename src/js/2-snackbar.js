9958670923d3feaf77eed71b29ca1290ee76ec5d
andriishmorgan <andrey.shmorgan@gmail.com>
2023-12-29 23:30:29 +0100

    upd

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,6 +1,8 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+AuthenticatorAttestationResponse
+
 const promiseForm = document.querySelector('form');
 const submitBtn = document.querySelector('button');
 
